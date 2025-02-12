342672e81cdfb03602032e656db3d5d6dcae0c14
Steliyan Stoyanov <steliyan@st6.io>
2025-02-12 08:50:26 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,24 @@
+# /mnt/etc/nixos/flake.nix
+{
+  inputs = {
+    # NOTE: Replace "nixos-24.11" with that which is in system.stateVersion of
+    # configuration.nix. You can also use latter versions if you wish to
+    # upgrade.
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    disko.url = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
+  };
+  outputs =
+    inputs@{ self, nixpkgs, ... }:
+    {
+      # NOTE: 'nixos' is the default hostname set by the installer
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        # NOTE: Change this to aarch64-linux if you are on ARM
+        system = "aarch64-linux";
+        modules = [
+          ./configuration.nix
+          inputs.disko.nixosModules.disko
+        ];
+      };
+    };
+}
